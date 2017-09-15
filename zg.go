0dc6205206c744508824b2465bc9496d31c23edc
dminGod <dmindesigns@gmail.com>
2017-09-15 23:58:07 +0700

    Added a sample pgxc_ctl.conf file

--- zg.go
+++ zg.go
@@ -6,28 +6,20 @@ import (
 	"fmt"
 )
 
-var AppConf config.ZgConfig
 
 func main() {
 
 
 	AppConf := config.GetConfig()
 
-	// fmt.Println(AppConf.Zgconf.Pgxcctl_conf_file);
 
 	k := pgctl_parser.Pgctl_parser{FileLocation: AppConf.Zgconf.Pgxcctl_conf_file}
 
 	k.Prase()
 
-
-//	fmt.Println(k.RawConfig)
-
 	for k, _ := range k.RawConfig {
 
 		fmt.Println( k )
 	}
-
-
-
-
 }
+
